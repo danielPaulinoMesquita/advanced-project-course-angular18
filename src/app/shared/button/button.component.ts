4fc58938ba4e1d78aeaa949d1eaf2b5d939f7479
Daniel Paulino <daniel-paulino@hotmail.com>
2025-06-29 21:14:04 -0300

    11- Using host for defining styles.

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -3,12 +3,12 @@ import { Component } from '@angular/core';
 // this a new kind of selector, you can define one for managing any element that has a property appButton.
 // actually any element with this property will be controlled by this component ButtonComponent
 @Component({
-  selector: 'button[appButton]',
+  selector: 'button[appButton], a[appButton]',
   standalone: true,
   imports: [],
   templateUrl: './button.component.html',
   styleUrl: './button.component.css'
 })
 export class ButtonComponent {
-
+// Another important concept is, every component has your own component
 }
--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -12,5 +12,7 @@ export class ControlComponent {
   label = input.required<string>();
 
   // encapsulation defines if global styles or local styles will affect this component as well.
-
+  // Another important thing in Angular, it's :host, also used in buttonComponent, it represents the component by itself
+  // and is good for not showing or declaring a name style class. You will notice in css file, the ".control" name was replaced by :host
+  // or another way for defining local style is leaving the configuration as ".control" and put that name on the "newTicketComponent" in the element "app-control"
 }
